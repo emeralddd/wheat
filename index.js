c6f70911e56f6f0b0e1741ed5f17f80fa401723f
emeralddd <66561741+emeralddd@users.noreply.github.com>
2021-11-13 14:26:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {ShardingManager} = require('discord.js')
 require('dotenv').config()
 
-const manager = new ShardingManager('./mainbot.js', { totalShards: 1, token: process.env.TOKEN })
+const manager = new ShardingManager('./mainbot.js', { totalShards: 2, token: process.env.TOKEN })
 
 manager.on('shardCreate', shard => console.log(`Shard ${shard.id} sinh thanh cong!`))
 
