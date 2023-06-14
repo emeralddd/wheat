05ed2548578674d83455e7cdec72f081dc20fa79
emeralddd <tunglambinbin@gmail.com>
2023-06-14 22:30:13 +0700

    fix database

--- modules/connectDatabase.js
+++ modules/connectDatabase.js
@@ -1,16 +1,17 @@
 const mongo = require('mongoose')
-require('dotenv').config({path: 'secret.env'})
+require('dotenv').config({ path: 'secret.env' })
 
 module.exports = async () => {
     try {
+        mongo.set("strictQuery", false);
         await mongo.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.${process.env.DBHOST}.mongodb.net/client?retryWrites=true&w=majority`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
         console.log('DB Connected Successfully!')
     } catch (err) {
-        if(err) {
+        if (err) {
             console.log(err.message)
             process.exit(1)
         }
