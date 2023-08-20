90d0ef0d53e04dea4e11303291bcbf04c51d65e3
emeralddd <tunglambinbin@gmail.com>
2023-08-20 16:37:09 +0700

    perf: update uptime format

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -1,13 +1,13 @@
 const { Client, Message, ChatInputCommandInteraction } = require('discord.js');
-const moment = require('moment')
-const bot = require('wheat-better-cmd')
-require('dotenv').config({path: 'secret.env'})
+const moment = require('moment');
+const bot = require('wheat-better-cmd');
+require('dotenv').config({ path: 'secret.env' });
 
 const help = {
-    name:"info",
-    group:"utility",
+    name: "info",
+    group: "utility",
     aliases: ["thongtin"]
-}
+};
 
 /**
  * @param {object} obj
@@ -17,32 +17,40 @@ const help = {
  * @param {Array} obj.helpMenu
  */
 
-const run = async ({wheat,message,interaction,lg}) => {
-    message||=interaction
-    const embed = bot.wheatSampleEmbedGenerate(true)
-    embed.setAuthor({name:`Wheat#1261`,iconUrl:process.env.AVATAR})
-    embed.setTitle(lg.main.aboutMe)
-    embed.setDescription(lg.main.botDescription)
-    const uptime_milli = moment.duration(wheat.uptime,'milliseconds')
+const run = async ({ wheat, message, interaction, lg }) => {
+    message ||= interaction;
+    const embed = bot.wheatSampleEmbedGenerate(true);
+    embed.setAuthor({ name: `Wheat#1261`, iconUrl: process.env.AVATAR });
+    embed.setTitle(lg.main.aboutMe);
+    embed.setDescription(lg.main.botDescription);
+    const uptime_milli = moment.duration(wheat.uptime, 'milliseconds');
     const promises = [
         await wheat.shard.fetchClientValues('guilds.cache.size'),
-    ]
+    ];
 
     const guildCount = await Promise.all(promises)
         .then(results => {
-				return results[0].reduce((acc, guildCount) => acc + guildCount, 0)
-			})
-		.catch(console.error)
+            return results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+        })
+        .catch(console.error);
 
-    const count=48;
+    const count = 48;
 
     const version = require('../../logs/overview.json').latest;
+
+    let uptime = "";
+
+    if (Math.floor(uptime_milli.asDays()) !== 0) uptime += ` ${Math.floor(uptime_milli.asDays())}d${Math.floor(uptime_milli.asDays()) === 1 ? '' : 's'}`;
+    if (Math.floor(uptime_milli.asHours()) !== 0) uptime += ` ${Math.floor(uptime_milli.asHours()) % 24}h${Math.floor(uptime_milli.asHours()) === 1 ? '' : 's'}`;
+    if (Math.floor(uptime_milli.asMinutes()) !== 0) uptime += ` ${Math.floor(uptime_milli.asMinutes()) % 60}min${Math.floor(uptime_milli.asMinutes()) === 1 ? '' : 's'}`;
+    if (Math.floor(uptime_milli.asSeconds()) !== 0) uptime += ` ${Math.floor(uptime_milli.asSeconds()) % 60}sec${Math.floor(uptime_milli.asSeconds()) === 1 ? '' : 's'}`;
+
 
     embed.addFields(
         {
             name: lg.main.generation,
             value: require(`../../logs/${version}.json`).gen,
-            inline:true
+            inline: true
         },
         {
             name: lg.main.version,
@@ -71,18 +79,18 @@ const run = async ({wheat,message,interaction,lg}) => {
         },
         {
             name: lg.main.uptime,
-            value: `${Math.floor(uptime_milli.asHours())} ${lg.main.hourS}, ${Math.floor(uptime_milli.asMinutes())%60} ${lg.main.minuteS}, ${Math.floor(uptime_milli.asSeconds())%60} ${lg.main.secondS}`,
+            value: uptime,
             inline: true
         },
         {
             name: lg.main.developer,
-            value: `temeralddd#1385`,
+            value: `darkemeralddd`,
             inline: true
         },
-    )
-    await bot.wheatEmbedSend(message,[embed])
+    );
+    await bot.wheatEmbedSend(message, [embed]);
 }
 
-module.exports.run = run
+module.exports.run = run;
 
-module.exports.help = help
+module.exports.help = help;
