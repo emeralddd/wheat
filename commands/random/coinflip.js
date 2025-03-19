c7302537e13e5affcc3a1756d701d14055be3c33
emeralddd <tunglambinbin@gmail.com>
2025-03-19 16:31:56 +1100

    chore(coinflip): add emoji for fliping

--- commands/random/coinflip.js
+++ commands/random/coinflip.js
@@ -12,10 +12,11 @@ const help = {
  */
 
 const run = async ({ request, lg }) => {
-    const cf = Math.floor(Math.random() * 1310);
-    await request.reply(lg.random.flipping);
+    const randomCoinFlipSeed = 1310;
+    const cf = Math.floor(Math.random() * randomCoinFlipSeed);
+    await request.reply('<a:coinfliping:1351788533999996949> ' + lg.random.flipping);
     setTimeout(() => {
-        request.edit(`${((cf & 1) ? lg.random.heads : lg.random.tails)}`);
+        request.edit(`${((cf & 1) ? '<:head:1351788558008188961> ' + lg.random.heads : '<:tail:1351788577121636353> ' + lg.random.tails)}!`);
     }, 2000);
 }
 
