212e12275a37b753e85a5996a358711708a9138d
austinnktl <s4070988@student.rmit.edu.au>
2025-04-08 22:35:26 +1000

    fix(apod): i18n and new fetch date for apod

--- commands/astronomy/apod.js
+++ commands/astronomy/apod.js
@@ -4,6 +4,7 @@ const axios = require('axios').default;
 require('dotenv').config({ path: 'secret.env' });
 const moment = require('moment');
 const { Request } = require('../../structure/Request');
+const { convertTo2DigitNumber } = require('../../modules/dateParse');
 
 const help = {
     name: "apod",
@@ -11,9 +12,25 @@ const help = {
     aliases: ['astropic', 'nasapic', 'picday'],
     rate: 3000,
     data: new SlashCommandBuilder()
-        .addStringOption(option =>
-            option.setName('date')
-                .setDescription('<DD/MM/YYYY>')
+        .addIntegerOption(option =>
+            option.setName('day')
+                .setDescription('day')
+                .setDescriptionLocalization('vi', "ngày")
+                .setMinValue(1)
+                .setMaxValue(31)
+        )
+        .addIntegerOption(option =>
+            option.setName('month')
+                .setDescription('month')
+                .setDescriptionLocalization('vi', "tháng")
+                .setMinValue(1)
+                .setMaxValue(12)
+        )
+        .addIntegerOption(option =>
+            option.setName('year')
+                .setDescription('year')
+                .setDescriptionLocalization('vi', "năm")
+                .setMinValue(1970)
         )
 }
 
@@ -22,53 +39,56 @@ const help = {
  * @param {Request} obj.request
  */
 
-const run = async ({ request, args, lg }) => {
-    const date = (request.isMessage ? args[1] : request.interaction.options.getString('date')) || moment().subtract(1, 'days').format('DD/MM/YYYY');
+const run = async ({ request, args, t }) => {
+    const [extractDay, extractMonth, extractYear] = dateInput(request, args ? args[1] : "", '/', ['day', 'month', 'year']);
 
-    const mmt = moment(date, 'DD/MM/YYYY', true);
-    if (!mmt.isValid()) {
-        await request.reply(lg.error.formatError);
-        return;
+    let mmt = null;
+
+    if ((request.isMessage && args.length === 1) || (request.isInteraction && (!extractDay || !extractMonth || !extractYear))) {
+        mmt = moment().subtract(1, 'days');
+    } else {
+        mmt = moment(`${convertTo2DigitNumber(extractDay)}/${convertTo2DigitNumber(extractMonth)}/${convertTo2DigitNumber(extractYear)}`, 'DD/MM/YYYY', true);
+        if (!mmt.isValid()) {
+            await request.reply(t('error.formatError'));
+            return;
+        }
+
+        if (mmt.isBefore('1995-06-16')) {
+            await request.reply(t('error.dateAfterApod'));
+            return;
+        }
+
+        if (mmt.isAfter(moment().subtract(1, 'days'))) {
+            await request.reply(t('error.dateOverApod'));
+            return;
+        }
     }
 
-    if (mmt.isBefore('1995-06-16')) {
-        await request.reply(lg.error.dateAfterApod);
-        return;
-    }
-
-    if (mmt.isAfter(moment().subtract(1, 'days'))) {
-        await request.reply(lg.error.dateOverApod);
-        return;
-    }
-
-    const date_str = mmt.format('YYYY-MM-DD');
+    const dateNasaFormat = mmt.format('YYYY-MM-DD');
 
     const key = [process.env.NASA_KEY1, process.env.NASA_KEY2, process.env.NASA_KEY3];
 
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${bot.wheatRandomElementFromArray(key)}&date=${date_str}`, {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${bot.wheatRandomElementFromArray(key)}&date=${dateNasaFormat}`, {
         timeout: 5000
     }).then(res => {
-        // console.log(res.data);
         if (!res.data.title) {
-            request.reply(lg.error.undefinedError);
-            return;
+            return request.reply(t('error.undefinedError'));
         }
 
         const embed = bot.wheatSampleEmbedGenerate();
 
         embed.setTitle(res.data.title);
-        embed.setFooter({ text: `${lg.main.copyright}: ${res.data.copyright || "NASA"}` });
+        embed.setFooter({ text: `${t('main.copyright')}: ${res.data.copyright || "NASA"}` });
         embed.setTimestamp(mmt.toDate());
         embed.setImage(res.data.url);
         embed.setDescription(res.data.explanation);
 
         request.reply({ embeds: [embed] });
     }).catch(error => {
-        // console.log(err);
         if (error.code === 'ECONNABORTED') {
-            request.reply(lg.error.nasaApodTakeTooLong);
+            request.reply(t('error.nasaApodTakeTooLong'));
         } else {
-            request.reply(lg.error.undefinedError);
+            request.reply(t('lg.error.undefinedError'));
         }
     });
 }
